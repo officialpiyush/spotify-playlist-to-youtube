3287ef2fc0c29344b6b935ac0ac01cfdfabc0812
Piyush Bhangale <bhangalepiyush@gmail.com>
2021-03-23 22:56:43 +0530

    create and add youtube playlit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import * as express from "express"
 import * as jwt from "jsonwebtoken"
 import * as cookieParser from "cookie-parser"
-import { google } from "googleapis"
+import { google, Auth } from "googleapis"
+import SpotifyParser from "./lib/SpotifyParser"
+import YoutubeConnector from "./lib/YoutubeConnector"
 import config = require("../config.json")
 
 const OAuth2 = google.auth.OAuth2
@@ -10,6 +12,7 @@ const authURL = oauthClient.generateAuthUrl({
     access_type: "offline",
     scope: config.creds.web.scopes
 })
+const parser = new SpotifyParser()
 
 const app = express()
 app.use(cookieParser())
@@ -39,6 +42,63 @@ app.get("/logged_in", async (req: express.Request, res: express.Response) => {
     if (!req.cookies.auth) return res.status(401).send("kek")
 
     res.send("logged in ")
+})
+
+app.post("/convert", async (req: express.Request, res: express.Response) => {
+    if (!req.cookies.auth) return res.status(401).send("kek")
+    const playlist = req.query.playlist
+
+    if (!playlist) res.status(400).send("no playlist")
+    const localOauthClient = new OAuth2(config.creds.web.client_id, config.creds.web.client_secret, config.creds.web.redirect_uris[0])
+    localOauthClient.credentials = jwt.verify(req.cookies.auth, config.secret) as Auth.Credentials
+
+    const playlistId = await parser.getID(playlist as string)
+    if (!playlistId) {
+        res.status(417).send("can't parse spotify playlist ID")
+        return
+    }
+
+    res.status(200).send("Started service, playlist shall be available in your youtube profile when completed")
+
+    const data = await parser.getTracks(playlistId as string)
+
+    const ytService = google.youtube("v3")
+
+    const playlistCreateData = await ytService.playlists.insert({
+        auth: localOauthClient,
+        part: [
+            "snippet"
+        ],
+        requestBody: {
+            snippet: {
+                title: data.name
+            },
+            status: {
+                privacyStatus: "unlisted"
+            }
+        }
+    })
+
+    for (const track of data.tracks) {
+        const t = await YoutubeConnector.searchTrack((await track).title, (await track).author)
+        if (t) {
+            ytService.playlistItems.insert({
+                auth: localOauthClient,
+                part: [
+                    "snippet"
+                ],
+                requestBody: {
+                    snippet: {
+                        playlistId: playlistCreateData.data.id,
+                        resourceId: {
+                            kind: "youtube#video",
+                            videoId: t
+                        }
+                    }
+                }
+            })
+        }
+    }
 })
 
 app.listen(config.port, () => {
--- src/lib/YoutubeConnector.ts
+++ src/lib/YoutubeConnector.ts
@@ -2,11 +2,10 @@ import * as ytsr from "ytsr"
 
 export default class YoutubeConnector {
     static async searchTrack (song: string, artist: string) {
-        const basicFilters = await ytsr.getFilters(`${song} ${artist}`)
-        const normalFilter = basicFilters.get("Type")?.get("Video")
+        const basicFilters = await (await ytsr.getFilters(`${song} ${artist}`)).get("Type")?.get("Video")
 
-        const resultData = await ytsr(normalFilter?.url!!)
+        const resultData = await ytsr(basicFilters?.url!!)
 
-        return (resultData.items.length <= 0 ? null : resultData.items[0].url)
+        return (resultData.items.length <= 0 ? null : resultData.items[0].id)
     }
 }
